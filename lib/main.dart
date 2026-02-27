1bfd1ddc7dce1bbc6c65ba0fa38db46b3eb66e83
avrsoftdev <avrsoftdev@gmail.com>
2026-02-27 14:48:09 +0530

    tessting push

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'src/bootstrap.dart';
 // Note: Firebase and Firebase App Check are initialized inside
 // `lib/src/bootstrap.dart`. App Check is configured to use the
 // debug provider during debug builds to make local development easier.
+//sieji
 Future<void> main() async {
   await bootstrap();
 }
